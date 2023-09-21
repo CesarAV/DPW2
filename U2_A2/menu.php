08bc1dc51b79d972dba02600f23f969924e67ddc
César A. V. Rodríguez <cesar.vargas@hemoeco.com>
2023-09-21 13:56:37 -0600

    Correct directory case

--- U2_A2/menu.php
+++ U2_A2/menu.php
@@ -1,7 +1,7 @@
 <nav class="menu-principal">
   <div class="menu">
     <div><a title="Inicio" href="/index.php">Inicio</a></div> 
-    <div><a title="Registro" href="/Usuario/crear.php">Registrarse</a></div>
+    <div><a title="Registro" href="/usuario/crear.php">Registrarse</a></div>
     <div><a title="Inicio de sesion" href="/iniciar-sesion.php">Iniciar sesi&oacute;n</a></div>
     <div><a href="/usuario/listado.php">Usuario</a></div>
   </div>

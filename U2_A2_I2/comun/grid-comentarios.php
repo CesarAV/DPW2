3923911ef6fd720656df2cadd1527fe718fcb987
César A. V. Rodríguez <cesar.av.rodriguez@gmail.com>
2024-03-27 10:58:31 -0600

    Agregar 'card'

--- U2_A2_I2/comun/grid-comentarios.php
+++ U2_A2_I2/comun/grid-comentarios.php
@@ -6,17 +6,17 @@
     ?>
     <h5>Comentarios de nuestros estudiantes</h5>
     <div class="<?= $colAlign ?> centrado">
-      <div class="frase">"Instalaciones de primer nivel"
+      <div class="frase card shadow">"Instalaciones de primer nivel"
         <div class="autor">María Conchita Figueroa, medicina</div>
       </div>
     </div>
     <div class="<?= $colAlign ?> centrado">
-      <div class="frase">"Maestros preparados con conocimientos actualizados"
+      <div class="frase card shadow">"Maestros preparados con conocimientos actualizados"
         <div class="autor">José Janín Vargas, filosofía</div>
       </div>
     </div>
     <div class="col-lg-4 col-md-12 centrado">
-      <div class="frase">"Un excelente instituto para aprender"
+      <div class="frase card shadow">"Un excelente instituto para aprender"
         <div class="autor">Elena Rodríguez, teología</div>
       </div>
     </div>
